eb6e71fe17e64f4678c19fe642608ca7714a93eb
Rob Halff <rob.halff@gmail.com>
2019-08-02 01:18:46 +0200

    export master datail container

--- lib/show.dart
+++ lib/show.dart
@@ -5,6 +5,7 @@ export 'src/layouts/grid_layout.dart';
 export 'src/layouts/index.dart';
 export 'src/layouts/showcase_layout.dart';
 export 'src/logger.dart';
+export 'src/master_detail/master_detail_container.dart';
 export 'src/show.dart';
 export 'src/showcase.dart';
 export 'src/showcase_item.dart';
