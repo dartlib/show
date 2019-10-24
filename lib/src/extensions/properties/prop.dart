3e8f8300f717df28fa32c875764bc67ab43fa366
Rob Halff <rob.halff@gmail.com>
2019-10-24 08:09:20 +0200

    do not rely on api prop of widget

--- lib/src/extensions/properties/prop.dart
+++ lib/src/extensions/properties/prop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:show/src/api/api.dart';
 import 'package:show/src/extensions/ui/widgets/showcase_item_widget.dart';
 
 import 'properties_extension.dart';
@@ -15,13 +16,10 @@ class Prop<T> {
     final showCaseItemWidget = context
         .ancestorWidgetOfExactType(ShowCaseItemWidget) as ShowCaseItemWidget;
 
-    final value =
-        showCaseItemWidget.api.getExtension<PropertiesExtension>().getProp<T>(
-              showCaseItemWidget.showCaseItem.id,
-              title,
-              defaultValue,
-            );
-
-    return value;
+    return Api.instance.getExtension<PropertiesExtension>().getProp<T>(
+          showCaseItemWidget.showCaseItem.id,
+          title,
+          defaultValue,
+        );
   }
 }
